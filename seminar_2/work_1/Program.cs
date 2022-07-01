7d1a81c580e9b2c091d747210943ee86b4656b68
Timur <arbo1234@yandex.ru>
2022-07-01 20:18:33 +0300

    Сделал первое задание

--- seminar_2/work_1/Program.cs
+++ seminar_2/work_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Random rnd = new Random();
+int number = rnd.Next(100, 1000);
+Console.WriteLine(number);
+
+int secondNumber = (number / 10) % 10;
+
+Console.WriteLine(secondNumber);
+
