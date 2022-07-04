b79200f8a50016e67fabb570d06070a90ace60f4
Timur <arbo1234@yandex.ru>
2022-07-04 20:57:56 +0300

    Поправил задачу № 3 во Второй домашке

--- seminar_2/work_3/Program.cs
+++ seminar_2/work_3/Program.cs
@@ -5,20 +5,10 @@
 // 1 -> нет
 
 Console.WriteLine("Введите день недели");
-string day = Console.ReadLine();
-if (day == "1")
-    Console.WriteLine("День не выходной");
-else if (day == "2")
-    Console.WriteLine("День не выходной");
-else if (day == "3")
-    Console.WriteLine("День не выходной");
-else if (day == "4")
-    Console.WriteLine("День не выходной");
-else if (day == "5")
-    Console.WriteLine("День не выходной");
-else if (day == "6")
-    Console.WriteLine("Выходной");
-else if (day == "7")
+int day = int.Parse(Console.ReadLine());
+if (day >= 1 && day <= 1)
+    Console.WriteLine("Будний");
+if (day < 5 && day > 7)
     Console.WriteLine("Выходной");
 else
-    Console.Write("Такого дня нет");
+    Console.WriteLine("Ввели чушь");
