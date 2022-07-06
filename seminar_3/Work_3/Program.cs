2db4f41fbc95efe73fd4a7b6b976325aedb758f5
Timur <arbo1234@yandex.ru>
2022-07-06 22:43:36 +0300

    Сделал задание № 3

---
+++ seminar_3/Work_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3-> 1, 8, 27
+// 5-> 1, 8, 27, 64, 125
+
+
+int GetNumber(string text)
+{
+    int number = 0;
+
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine(text);
+        if (int.TryParse(Console.ReadLine(), out number)) break;
+        else
+        {
+            Console.WriteLine(text);
+        }
+    }
+    return number;
+}
+void Sqrt(int result)
+{
+    for (int i = 1; i < result; i++)
+    {
+        Console.WriteLine(Math.Pow(i, 2));
+    }
+}
+int result = GetNumber("Введиче число");
+Sqrt(result);
