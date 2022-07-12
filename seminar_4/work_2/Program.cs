8cdc1681329e3ae1574f28444d3977a8d2c60c0d
Timur <arbo1234@yandex.ru>
2022-07-12 22:30:36 +0300

    Сделала задание 2

---
+++ seminar_4/work_2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+void GetNumber(string text)
+{
+    int number = 0;
+    Console.WriteLine(text);
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out number)) break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(text);
+        }
+    }
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        sum = sum + digit;
+        number = number / 10;
+    }
+    Console.WriteLine(sum);
+}
+GetNumber("Напишите число и получите сумму чисел в числе");
