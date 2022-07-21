9431c7461ab23ef4030f13b9c1b77a42723a31dc
Timur <arbo1234@yandex.ru>
2022-07-21 20:36:41 +0300

    Сделал задание 1

---
+++ seminar_7/worl_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void GetMatrix(int m, int n)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix[i, j] = rnd.Next(1, 10);
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.WriteLine("Введите количетсов строк массива");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество стобцов массива");
+int n = int.Parse(Console.ReadLine());
+GetMatrix(m, n);
